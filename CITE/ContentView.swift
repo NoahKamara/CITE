6d17b6b3ea2580c23e5d855b2f4d8add71cb1ebe
Noah Kamara <developer@noahkamara.com>
2020-07-18 18:47:12 +0200

    Initial Commit

---
+++ CITE/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CITE
+//
+//  Created by Noah Kamara on 18.07.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
